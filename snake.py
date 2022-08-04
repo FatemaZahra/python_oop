c5033e747858ba97d76930686a6d64fe7d92b289
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-08-04 14:28:43 +0100

    exception handling

--- snake.py
+++ snake.py
@@ -8,8 +8,12 @@ class Snake(Reptile):
         super().__init__()
         self.forked_tongue = True
 
-    def use_tounge_to_smell(self):
-        return "I can use my tongue to smell food"
+    def _use_tounge_to_smell(self):
+        try:
+            return snake_object._use_tongue_to_smell()
+
+        except AttributeError:
+            return "this information is protected"
 
     # create 2 more functions one with _ the other with __
     # execute them both - return message should explain Encapsulation breakdown - public -protected - private
@@ -25,7 +29,7 @@ snake_object = Snake()
 
 print(snake_object.eat()) # this function is inherited from Animal
 print(snake_object.seek_heat()) #this function is inherited from Reptile class
-print(snake_object.use_tounge_to_smell()) # public
+print(snake_object._use_tounge_to_smell()) # public
 print(snake_object._snake_hunt()) # protected
 # print(snake_object.__venom()) # private
 
